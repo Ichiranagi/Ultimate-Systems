9d22532d99ce885583833eb17e48dcf7202276fa
Alexis V. <Alexisvla0177@gmail.com>
2023-11-24 19:19:15 -0400

    Estructuracion de proyecto

---
+++ Airsystem.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+#include <math.h>
+#include <conio.h>
+#include <cstdio>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
